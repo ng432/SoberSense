f33ee7f2bbfafcd8b2167a75a6a7d19ef5080bde
Nicholas Gregory <ng432@cam.ac.uk>
2023-10-29 13:37:39 +0000

    Compartmentalising time selection

---
+++ SoberSenseApp/SoberSense/StartingScreen/TimePicker.swift
@@ -0,0 +1,36 @@
+//
+//  GenderPicker.swift
+//  SoberSense
+//
+//  Created by nick on 29/10/2023.
+//
+
+import SwiftUI
+
+struct TimePicker: View {
+    @Binding var selectedHour: Int
+    @Binding var selectedMinute: Int
+
+    var body: some View {
+        HStack {
+            Text("Time since first drink:")
+            
+            VStack(spacing: 0) {
+                Picker("Number", selection: $selectedHour) {
+                    ForEach(0 ..< 11, id: \.self) { number in
+                        Text("\(number) hours")
+                    }
+                }
+                .pickerStyle(DefaultPickerStyle())
+                
+                Picker("Number", selection: $selectedMinute) {
+                    ForEach(0 ..< 6, id: \.self) { number in
+                        Text("\(number * 10) minutes")
+                    }
+                }
+                .pickerStyle(DefaultPickerStyle())
+            }
+        }
+    }
+}
+

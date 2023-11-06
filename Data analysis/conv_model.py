5b472980944f383bd18ca99f48b16eacc5530879
Nicholas Gregory <ng432@cam.ac.uk>
2023-11-06 20:56:28 +0000

    renaming class for model, and reorganising

--- Data analysis/conv_model.py
+++ Data analysis/conv_model.py
@@ -12,32 +12,13 @@ from torch.utils.data import DataLoader, random_split
 device = "cuda" if t.cuda.is_available() else "mps" if t.backends.mps.is_available() else "cpu"
 print(f"Using {device} device")
 
-#%%
-
-data_set = SoberSenseDataset(
-    "sample_data",
-    sample_transform=sample_transform,
-    label_transform=lambda x: t.tensor(x, dtype=t.float32).to(device),
-    animation_interp_number=60,
-)
-
-train_ratio = 0.8
-test_ratio = 1 - train_ratio
-
-train_size = int(train_ratio * len(data_set))
-test_size = len(data_set) - train_size
-
-train_dataset, test_dataset = random_split(data_set, [train_size, test_size])
-
-train_dataloader = DataLoader(train_dataset, batch_size=4)
-test_dataloader = DataLoader(test_dataset, batch_size=4)
 
 
 #%%
 
-class CNNmodel(nn.Module):
+class regressionCNN(nn.Module):
     def __init__(self, input_channels, num_samples):
-        super(CNNmodel, self).__init__()
+        super(regressionCNN, self).__init__()
 
         # Size of sample data is B, 2, 3, 1525
         # B: batch size 
@@ -47,11 +28,13 @@ class CNNmodel(nn.Module):
 
         self.ReLU = nn.ReLU()
 
-        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=(3, 10), padding = 'same')
+        conv1_ks = (1,10)
+
+        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size = conv1_ks, padding = 'same')
         # Sample data: will give shape (B, 32, 3, 1525)
 
-        # will convolve along the time dimension 
-        maxpool1_ks = (1,5)
+        # will extract along the time dimension 
+        maxpool1_ks = (1,25)
 
         # Sample data: input (B, 32, 3, 1525)
         self.maxpool1 = nn.MaxPool2d(kernel_size = maxpool1_ks)
@@ -106,7 +89,25 @@ class CNNmodel(nn.Module):
 
         return x
     
+#%%
 
+data_set = SoberSenseDataset(
+    "sample_data",
+    sample_transform=sample_transform,
+    label_transform=lambda x: t.tensor(x, dtype=t.float32).to(device),
+    animation_interp_number=60,
+)
+
+train_ratio = 0.8
+test_ratio = 1 - train_ratio
+
+train_size = int(train_ratio * len(data_set))
+test_size = len(data_set) - train_size
+
+train_dataset, test_dataset = random_split(data_set, [train_size, test_size])
+
+train_dataloader = DataLoader(train_dataset, batch_size=4)
+test_dataloader = DataLoader(test_dataset, batch_size=4)
 
 #%%
 
@@ -121,7 +122,7 @@ loss_fn = nn.MSELoss()
 # number of samples per recordin 
 num_samples = data_set[0][0].shape[2]
 
-cnn_model = CNNmodel(
+cnn_model = regressionCNN(
     input_channels = 2, 
     num_samples = num_samples).to(device)
 
@@ -129,7 +130,7 @@ optimizer = t.optim.SGD(cnn_model.parameters(), lr=learning_rate)
 
 #%%
 
-epochs = 100
+epochs = 200
 for i in range(epochs):
     print(f"Epoch {i+1}\n-------------------------------")
     train_loop(train_dataloader, cnn_model, loss_fn, optimizer)

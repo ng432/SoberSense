8c198f9251155b1c2adb914db7b114670350679e
Nicholas Gregory <ng432@cam.ac.uk>
2023-11-19 14:18:08 +0000

    added cache for prep transforms

--- Data analysis/unpacking_data.py
+++ Data analysis/unpacking_data.py
@@ -10,14 +10,21 @@ import numpy as np
 
 
 class SoberSenseDataset(Dataset):
+
     def __init__(
-        self, data_folder, device="mps", sample_transform=None, label_transform=None):
+        self, data_folder, device="mps", prep_transform=None, augmentation_transform=None, label_transform=None):
+
+        if augmentation_transform is not None and prep_transform is None:
+            raise ValueError("Augmentation transform provided without a preparatory transform.")
+
         self.data_folder = data_folder
         self.device = device
-        self.sample_transform = sample_transform
+        self.prep_transform = prep_transform
+        self.prep_cache = {} # prep transform can be expensive to compute, so cache to store results
+        self.augmentation_transform = augmentation_transform
         self.label_transform = label_transform
         self.samples = []  # A list to store the data examples
-
+        
         # Iterate through JSON files in the data folder
         for filename in os.listdir(data_folder):
             if filename.endswith(".json"):
@@ -42,9 +49,9 @@ class SoberSenseDataset(Dataset):
                         sample_data["touchData"],
                         "xLocation",
                         "yLocation",
-                        start_time=sample_data["randomPath"][0]["time"],
-                        scale=scale,
-                        game_length=game_length,
+                        start_time = sample_data["randomPath"][0]["time"],
+                        scale = scale,
+                        game_length = game_length,
                     )
 
                     # Random path data is recorded as scaled, but from -0.5 to 0.5
@@ -72,10 +79,20 @@ class SoberSenseDataset(Dataset):
         return len(self.samples)
 
     def __getitem__(self, idx):
-        sample_data, label = self.samples[idx]
 
-        if self.sample_transform:
-            sample_data = self.sample_transform(sample_data)
+        if idx in self.prep_cache: # if already applied prep transform
+            sample_data = self.prep_cache[idx]
+        elif self.prep_transform: # if haven't applied prep transform, and it exists
+            sample_data, _ = self.samples[idx]
+            sample_data = self.prep_transform(sample_data)
+            self.prep_cache[idx] = sample_data
+        else: # if no prep transform
+            sample_data, _ = self.samples[idx]
+
+        _ , label = self.samples[idx]
+
+        if self.augmentation_transform:
+            sample_data = self.augmentation_transform(sample_data)
             sample_data = sample_data.to(self.device)
 
         if self.label_transform:
@@ -89,7 +106,6 @@ def UnpackCoordData(data, x_name, y_name, start_time, scale=None, shift=0, game_
     # If scale provided, scale the touch data by width or height
 
     x = [point[x_name] / scale["x_scale"] if scale else point[x_name] + shift for point in data]
-
     y = [point[y_name] / scale["y_scale"] if scale else point[y_name] + shift for point in data]
 
     time = [(point["time"] - start_time) / game_length for point in data]

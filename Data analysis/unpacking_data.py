e65e6dc8355aa255047ebfb8245fc54b2ea696d3
Nicholas Gregory <ng432@cam.ac.uk>
2023-12-05 16:15:57 +0000

    pre-processing distance scale now consistent

--- Data analysis/unpacking_data.py
+++ Data analysis/unpacking_data.py
@@ -4,15 +4,17 @@
 
 import json
 import os
-import torch as t
 from torch.utils.data import Dataset
-import numpy as np
 
 
 class SoberSenseDataset(Dataset):
-
+    
     def __init__(
-        self, data_folder, device="mps", prep_transform=None, augmentation_transform=None, label_transform=None):
+        self, data_folder, label_name='unitsDrunk', device="mps", screenScaling = 926, length_threshold = 0,
+        prep_transform=None, augmentation_transform=None, label_transform=None):
+        # screen scaling is hard coded at 926
+        # this represents the largest height possible of an iPhone (model iPhone 14 Pro)
+        # ths value is in 'points', a measurement defined by Swift/Apple as to have consistent measurements across iPhone models
 
         if augmentation_transform is not None and prep_transform is None:
             raise ValueError("Augmentation transform provided without a preparatory transform.")
@@ -23,7 +25,7 @@ class SoberSenseDataset(Dataset):
         self.prep_cache = {} # prep transform can be expensive to compute, so cache to store results
         self.augmentation_transform = augmentation_transform
         self.label_transform = label_transform
-        self.samples = []  # A list to store the data examples
+        self.samples = []  # A list to store the data examples and labels
         
         # Iterate through JSON files in the data folder
         for filename in os.listdir(data_folder):
@@ -32,48 +34,53 @@ class SoberSenseDataset(Dataset):
 
                     sample_data = json.load(json_file)
 
-                    if "duration" in sample_data:
-                        # time, in seconds, that the game runs over
-                        game_length = sample_data["duration"]
-                    # for sample_data, where data doesn't have 'duration', but was 20 seconds
-                    else:
-                        game_length = 20
+                    # poorly recorded data can have limited touch points
+                    # threshold used to ignore samples with too few touch points recorded
+                    if len(sample_data["touchData"]) > length_threshold:
+        
+                        # duration used to scale times
+                        if "duration" in sample_data:
+                            # time, in seconds, that the game runs over
+                            game_length = sample_data["duration"]
+                        # for collected data where data doesn't have explicit 'duration'
+                        else:
+                            game_length = 20
 
-                    scale = {
-                        "x_scale": sample_data["screenSize"]["width"],
-                        "y_scale": sample_data["screenSize"]["height"],
-                    }
+                        # note using the same scaling for x and y values as to have only one scale for distance
+                        screen_dim = {
+                            "width": sample_data["screenSize"]["width"],
+                            "height": sample_data["screenSize"]["height"]
+                        }
 
-                    # This will output scaled touch data (from 0 to 1)
-                    unpacked_touchData = UnpackCoordData(
-                        sample_data["touchData"],
-                        "xLocation",
-                        "yLocation",
-                        start_time = sample_data["randomPath"][0]["time"],
-                        scale = scale,
-                        game_length = game_length,
-                    )
+                        # This will output scaled touch data
+                        unpacked_touchData = UnpackTouchData(
+                            sample_data["touchData"],
+                            start_time = sample_data["randomPath"][0]["time"],
+                            scale = screenScaling,
+                            game_length = game_length,
+                        )
 
-                    # Random path data is recorded as scaled, but from -0.5 to 0.5
-                    # Hence, shift = 0.5 to give normalised coordinates from 0 to 1
-                    unpacked_randomPath = UnpackCoordData(
-                        sample_data["randomPath"],
-                        "xDimScale",
-                        "yDimScale",
-                        start_time=sample_data["randomPath"][0]["time"],
-                        shift=0.5,
-                        game_length=game_length,
-                    )
+                        # Random path data is recorded as scaled, but from -0.5 to 0.5
+                        # Hence, shift = 0.5 to give normalised coordinates from 0 to 1
+                        unpacked_randomPath = UnpackPathData(
+                            sample_data["randomPath"],
+                            scale = screenScaling,
+                            screen_dim = screen_dim,
+                            start_time = sample_data["randomPath"][0]["time"],
+                            shift=0.5,
+                            game_length=game_length,
+                        )
 
-                    label = sample_data["unitsDrunk"]
+                        label = sample_data[label_name]
 
-                    sample = {"touchData": unpacked_touchData,
-                               "randomPath": unpacked_randomPath,
-                               "controlPoints": sample_data["controlPoints"],
-                               "animationDuration": sample_data["animationDuration"],
-                               "gameLength": game_length}
-                    
-                    self.samples.append((sample, label))
+                        sample = {"touchData": unpacked_touchData,
+                                "randomPath": unpacked_randomPath,
+                                "screenHeight": sample_data["screenSize"]["height"],
+                                "controlPoints": sample_data["controlPoints"],
+                                "animationDuration": sample_data["animationDuration"],
+                                "gameLength": game_length}
+                        
+                        self.samples.append((sample, label))
 
     def __len__(self):
         return len(self.samples)
@@ -102,11 +109,26 @@ class SoberSenseDataset(Dataset):
         return sample_data, label
 
 
-def UnpackCoordData(data, x_name, y_name, start_time, scale=None, shift=0, game_length=1):
-    # If scale provided, scale the touch data by width or height
+def UnpackTouchData(data, start_time, scale=None, game_length=1):
 
-    x = [point[x_name] / scale["x_scale"] if scale else point[x_name] + shift for point in data]
-    y = [point[y_name] / scale["y_scale"] if scale else point[y_name] + shift for point in data]
+    x = [point['xLocation'] / scale for point in data]
+    y = [point['yLocation'] / scale for point in data]
+
+    time = [(point["time"] - start_time) / game_length for point in data]
+
+    unpacked_data = {"X": x, "Y": y, "time": time}
+
+    return unpacked_data
+
+def UnpackPathData(data , start_time, scale=None, screen_dim=None, shift=0, game_length=1):
+    # raw X and Y is scaled from -0.5 to 0.5, representing percentage of that specific device's screen
+    # so scaling needs to account for this
+
+    x_factor = screen_dim['width'] / scale
+    y_factor = screen_dim['height'] / scale
+
+    x = [(point['xDimScale'] + shift) * x_factor for point in data]
+    y = [(point['yDimScale'] + shift) * y_factor for point in data]
 
     time = [(point["time"] - start_time) / game_length for point in data]
 
@@ -116,6 +138,9 @@ def UnpackCoordData(data, x_name, y_name, start_time, scale=None, shift=0, game_
 
 
 
+
+
+# %%
 
 
 # %%

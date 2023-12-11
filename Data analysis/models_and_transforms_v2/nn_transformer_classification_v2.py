054094d20ed5dfc9e2164eb19fbed980ecd5b799
Nicholas Gregory <ng432@cam.ac.uk>
2023-12-11 09:47:59 +0000

    updating to classification

--- Data analysis/models_and_transforms_v2/nn_transformer_classification_v2.py
+++ Data analysis/models_and_transforms_v2/nn_transformer_classification_v2.py
@@ -3,7 +3,7 @@
 
 import sys
 import os
-from data_transforms_v2 import prep_transform, randomly_flipx, randomly_flipy, append_distance, randomly_crop
+from data_transforms_v2 import prep_transform, randomly_flipx, randomly_flipy, append_distance, randomly_crop, binary_label_transform
 import torch as t
 
 from torch import nn
@@ -22,7 +22,7 @@ print(f"Using {device} device")
 
 # %%
 
-class TransformerRegressor(nn.Module):
+class TransformerBC(nn.Module):
     def __init__(self, n_features, n_heads, n_hidden, n_layers, dropout):
         super().__init__()
 
@@ -35,6 +35,9 @@ class TransformerRegressor(nn.Module):
         self.transformer = nn.TransformerEncoder(transformer_layer, num_layers=n_layers)
         self.decoder = nn.Linear(n_hidden, 1)
 
+        self.sigmoid = nn.Sigmoid()
+
+
     def forward(self, x):
 
         # input is of shape [B, num_features, seq_length]
@@ -44,28 +47,46 @@ class TransformerRegressor(nn.Module):
         x = t.transpose(x, -1, -2)
         x = self.encoder(x)
 
+        x = t.transpose(x, 0, 1)
+        
         x = self.transformer(x)
+
         x = self.decoder(x[-1])
+
+        x = self.sigmoid(x)
+  
+        
         return x
     
 #%%
 
-sample_data_path = os.path.join(parent_dir, 'sample_data')
 
 def augmentation_transform(x):
-    x = randomly_crop(x, crop_size=100)
+    x = randomly_crop(x, crop_size=250)
     x = append_distance(x)
     x = randomly_flipx(x)
     x = randomly_flipy(x)
     return x
 
+def binary_label_transform(label, threshold = 0.07):
+    # 1 represents 
+    if label > threshold:
+        label = t.tensor([1], dtype=t.float32).to(device)
+    else:
+        label = t.tensor([0], dtype=t.float32).to(device)
+
+    return label
+
+data_path = os.path.join(parent_dir, 'pilot_data')
 
 # test_data has 40 samples, with either 0, 5 or 10 units drunk, and simulated 'drunk behaviour'
 data_set = SoberSenseDataset(
-    sample_data_path,
+    data_path,
+    label_name='BAC', 
     prep_transform=prep_transform,
     augmentation_transform= augmentation_transform,
-    label_transform=lambda x: t.tensor(x, dtype=t.float32).to(device)
+    label_transform=binary_label_transform,
+    length_threshold=300
 )
 
 #%%
@@ -74,32 +95,36 @@ data_shape = data_set[0][0].shape
 n_features = data_shape[0]
 
 n_heads = 12
-n_hidden = 128
+n_hidden = 144
 n_layers = 6
 dropout = 0.1
 
-model = TransformerRegressor(n_features, n_heads, n_hidden, n_layers, dropout).to(device)
+model = TransformerBC(n_features, n_heads, n_hidden, n_layers, dropout).to(device)
+
 
 # %%
 
 
-train_ratio = 0.6
+train_ratio = 0.75
 test_ratio = 1 - train_ratio
 train_size = int(train_ratio * len(data_set))
 test_size = len(data_set) - train_size
 train_dataset, test_dataset = random_split(data_set, [train_size, test_size])
 
-batch_size = 4
+
+#%%
+
+batch_size = 8
 
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
 
-loss_fn = nn.MSELoss()
+loss_fn = nn.BCELoss()
 
 learning_rate = 3e-3
 optimizer = t.optim.SGD(model.parameters(), lr=learning_rate)
 
-epochs = 1000
+epochs = 60000
 writer = SummaryWriter()
 
 for i in range(epochs):
